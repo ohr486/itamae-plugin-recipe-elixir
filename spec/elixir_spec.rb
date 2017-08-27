333049267639f6c8e80a9ca687a66c4541687474
ohr486 <ohr486@gmail.com>
2017-08-27 17:47:42 +0900

    remove erlang spec

--- spec/elixir_spec.rb
+++ spec/elixir_spec.rb
@@ -1,17 +1,8 @@
 require "spec_helper"
 
-describe file("/root/.kerlrc") do
-  it { should be_file }
-end
-
 describe file("/etc/profile.d/elixir.sh") do
   it { should be_file }
   its(:content) { should match /1\.5\.1/ }
-end
-
-describe file("/usr/bin/kerl") do
-  it { should be_file }
-  it { should be_executable }
 end
 
 describe command("source /etc/profile.d/kerl.sh && \
