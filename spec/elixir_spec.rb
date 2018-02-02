9e551f387654a544c7ded214992c9fb5ffdc28bc
ohr486 <ohr486@gmail.com>
2018-02-02 21:05:36 +0900

    update default ver

--- spec/elixir_spec.rb
+++ spec/elixir_spec.rb
@@ -2,11 +2,11 @@ require "spec_helper"
 
 describe file("/etc/profile.d/elixir.sh") do
   it { should be_file }
-  its(:content) { should match /1\.5\.1/ }
+  its(:content) { should match /1\.6\.1/ }
 end
 
 describe command("source /etc/profile.d/kerl.sh && \
                   source /etc/profile.d/elixir.sh && \
                   iex -v") do
-  its(:stdout) { should contain("1.5.1") }
+  its(:stdout) { should contain("1.6.1") }
 end
